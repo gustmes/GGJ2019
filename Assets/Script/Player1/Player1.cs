70d0baffa2fffb28c76a8e8b7c3cd582e91cd346
Ramon Vicente <ramonvicentee@gmail.com>
2019-01-26 10:07:35 -0300

    initital commit

---
+++ Assets/Script/Player1/Player1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D rigidBody;
+	private Vector2 moveVelocity;
+	// Use this for initialization
+	void Start () {
+
+		rigidBody = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		moveVelocity = moveInput.normalized * speed;
+		
+	}
+
+	void FixedUpdate(){
+		rigidBody.MovePosition(rigidBody.position + moveVelocity * Time.fixedDeltaTime);
+	}
+}
