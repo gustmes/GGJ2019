6fd3a738b1ed5d3684e52fa7e6bea5296ec6a8c4
Ramon Vicente <ramonvicentee@gmail.com>
2019-01-26 11:15:12 -0300

    foodbar created

---
+++ Assets/Script/Player1/FoodBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FoodBar : MonoBehaviour {
+
+	public Slider foodBar;
+	public float maxHealth;
+	public float starve;
+
+	// Use this for initialization
+	void Start () {
+		foodBar = GetComponent<Slider>();
+
+		foodBar.maxValue = maxHealth;
+		foodBar.value = starve;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		starve+=0.1f;
+ 		foodBar.value = starve;
+	}
+}
