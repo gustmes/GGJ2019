65723478a1b13c08df753ed882984305198789a1
gustmes <gustmes@hotmail.com>
2019-01-26 11:05:37 -0300

    Adding Food (player can consume food)

---
+++ Assets/Script/Food/FoodSpawn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawn : MonoBehaviour {
+
+    [SerializeField] Food foodPrefab;
+    [SerializeField] float minFoodSpawnRate;
+    [SerializeField, Range(0f, 100f)] float maxFoodSpawnRate;
+
+    [SerializeField] BoxCollider2D levelBox;
+
+    float timeToSpawn;
+    float timer = 0f;
+
+    private void Start()
+    {
+        timeToSpawn = (minFoodSpawnRate <= 0f) ? maxFoodSpawnRate : Random.Range(minFoodSpawnRate, maxFoodSpawnRate);
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timeToSpawn)
+        {
+            SpawnFood();
+            timer = 0f;
+
+            timeToSpawn = (minFoodSpawnRate <= 0f) ? maxFoodSpawnRate : Random.Range(minFoodSpawnRate, maxFoodSpawnRate);
+        }
+    }
+
+    void SpawnFood()
+    {
+        Debug.Log("Spawn food");
+    }
+}
